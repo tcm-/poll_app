18e8b486b95c23f0a5f5a1355d146271a550e6f5
tcm <admim@tcm.onl>
2014-09-09 16:39:22 -0400

    Added models to admin

--- polls/admin.py
+++ polls/admin.py
@@ -1,6 +1,14 @@
 from django.contrib import admin
-from polls.models import Question
+from polls.models import Choice, Question
 
 # Register your models here.
-admin.site.register(Question)
+
+class QuestionAdmin(admin.ModelAdmin):
+	fieldsets = [
+		(None, {'fields': ['question_text']}),
+		('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
+	]
+
+admin.site.register(Question, QuestionAdmin)
+admin.site.register(Choice)
 
