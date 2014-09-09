eb24b02b5d2ceaf6a216f948e668f3b833ab17b9
tcm <admim@tcm.onl>
2014-09-09 17:00:17 -0400

    Updated Question model

--- polls/admin.py
+++ polls/admin.py
@@ -14,11 +14,6 @@ class QuestionAdmin(admin.ModelAdmin):
 	]
 	inlines = [ChoiceInline]
 	list_display = ('question_text', 'pub_date', 'was_published_recently')
-	def was_published_recently(self):
-		return self.pub_date >= timezone.new() - datetime.timedelta(days=1)
-	was_published_recently.admin_order_field = 'pub_date'
-	was_published_recently.boolean = True
-	was_published_recently.short_description = 'Published recently?'
 	list_filter = ['pub_date']
 	search_fields = ['question_date']
 
