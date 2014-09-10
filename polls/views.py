2781ceeedb5024b84a4b1ada9cde005712cf2ad3
tcm <admim@tcm.onl>
2014-09-10 14:38:07 -0400

    Converted existing views to generic views

--- polls/views.py
+++ polls/views.py
@@ -1,29 +1,40 @@
 from django.http import HttpResponseRedirect, HttpResponse
-from django.http import Http404
-from django.shortcuts import get_object_or_404
-from django.shortcuts import render
+#from django.http import Http404
+from django.shortcuts import get_object_or_404, render
+#from django.shortcuts import render
 from django.core.urlresolvers import reverse
+from django.views import generic
+
 from polls.models import Choice, Question
 
 # Create your views here.
 
-def index(request):
-	latest_question_list = Question.objects.all().order_by('-pub_date')[:5]
-	context = {'latest_question_list' : latest_question_list}
-	return render(request, 'polls/index.html', context)
+class IndexView(generic.ListView):
+	#latest_question_list = Question.objects.all().order_by('-pub_date')[:5]
+	#context = {'latest_question_list' : latest_question_list}
+	#return render(request, 'polls/index.html', context)
+	template_name = 'polls/index.html'
+	context_object_name='latest_question_list'
 
-def detail(request, question_id):
+	def get_queryset(self):
+		return Question.objects.order_by('-pub_date')[:5]
+
+class DetailView(generic.DetailView):
 	#try:
 	#	question = Question.objects.get(pk=question_id)
 	#except Question.DoesNotExist:
 	#	raise Http404
 	#return render(request, 'polls/detail.html', {'question': question})
-	question = get_object_or_404(Question, pk=question_id)
-	return render(request, 'polls/detail.html', {'question': question})
+	#question = get_object_or_404(Question, pk=question_id)
+	#return render(request, 'polls/detail.html', {'question': question})
+	model = Question
+	template_name = 'polls/detail.html'
 
-def results(request, question_id):
-	response ="You're looking at the results of question %s."
-	return HttpResponse(response % question_id)
+class ResultsView(generic.DetailView):
+	#response ="You're looking at the results of question %s."
+	#return HttpResponse(response % question_id)
+	model = Question
+	template_name = 'polls/results.html'
 
 def vote(request, question_id):
 #	return HttpResponse("You're voting on question %s" % question_id)
